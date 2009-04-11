fc9f2035b650baabaf39507d5dfe7bb7e6ca3671
Jannis Leidel <jannis@leidel.info>
2009-04-12 01:26:53 +0200

    Added a bunch of default templates to get started more easily, fixed the wsgi file with a shebang and such

--- jzdz.wsgi
+++ jzdz.wsgi
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'jzdz.settings'
 
--- settings.py
+++ settings.py
@@ -12,12 +12,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'jzdz.db'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = os.path.join(PROJECT_DIR, 'jzdz.db')
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -58,6 +54,10 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
     'dbtemplates.loader.load_template_source',
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_DIR, 'templates'),
 )
 
 MIDDLEWARE_CLASSES = (
