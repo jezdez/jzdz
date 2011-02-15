69ad41b49f1153a5a36db2ed883240e120e811ce
Jannis Leidel <jannis@leidel.info>
2011-02-15 14:29:36 +0100

    Added basic staticfiles settings.

--- jzdz/settings.py
+++ jzdz/settings.py
@@ -42,17 +42,21 @@ USE_I18N = False
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_DIR, os.par, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/admin/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, os.pardir, 'static')
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'SERIOUSLY_DUDE_CHANGE_THIS_IN_YOUR_INSTALL'
