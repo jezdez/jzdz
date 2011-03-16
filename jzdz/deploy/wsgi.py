76bda64d27addf55505b8085127874bb3e0ead7a
Jannis Leidel <jannis@leidel.info>
2011-03-16 15:53:47 -0400

    Added project to sys.path.

--- jzdz/deploy/wsgi.py
+++ jzdz/deploy/wsgi.py
@@ -1,7 +1,10 @@
-import os
+import os, sys
 
 from django.conf import settings
 from django.core.handlers.wsgi import WSGIHandler
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.pardir))
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "jzdz.settings"
 
