412e7309cf83a0177451c5d881cb21242b106808
Jannis Leidel <jannis@leidel.info>
2011-04-15 12:18:33 +0200

    Updated staticfiles.

--- jzdz/urls.py
+++ jzdz/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
-
 from django.contrib import admin
 admin.autodiscover()
+
+from staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
+    urlpatterns += staticfiles_urlpatterns() + patterns('',
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT}),
     )
